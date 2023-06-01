1288164c1bb22a755df655e1e9ce4cbe228c24dd
1167824 <1167824@JC392020.ifrn.local>
2023-06-01 11:46:53 -0300

    Funcionalidade de data de nascimento, solves #1

--- app/models/Pessoa.java
+++ app/models/Pessoa.java
@@ -3,6 +3,9 @@ package models;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import play.db.jpa.Model;
 
 @Entity
@@ -11,6 +14,9 @@ public class Pessoa extends Model {
 	public String nome;
 	public String email;
 	public String senha;
+	
+	@Temporal(TemporalType.DATE)
+	public Date nascimento;
 
 	@Override
 	public String toString() {

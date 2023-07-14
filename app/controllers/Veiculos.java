75ff5ede9fe7c5b77ea5ebc56a578e2b4318688f
1167824 <1167824@JC392020.ifrn.local>
2023-07-14 10:11:48 -0300

    Seleção de proprietário no form de veículo

--- app/controllers/Veiculos.java
+++ app/controllers/Veiculos.java
@@ -2,13 +2,15 @@ package controllers;
 
 import java.util.List;
 
+import models.Pessoa;
 import models.Veiculo;
 import play.mvc.Controller;
 
 public class Veiculos extends Controller {
 	
 	public static void form() {
-		render();
+		List<Pessoa> pessoas = Pessoa.findAll();
+		render(pessoas);
 	}
 	
 	public static void salvar(Veiculo v) {

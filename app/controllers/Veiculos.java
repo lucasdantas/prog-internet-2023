43993f890eeadc7a5e69b6f062bf70741a8d2b1b
Lucas Dantas <lucashiagod@gmail.com>
2023-11-30 09:58:05 -0300

    Edição e validação de formulário auxiliado por flash

--- app/controllers/Veiculos.java
+++ app/controllers/Veiculos.java
@@ -17,6 +17,16 @@ public class Veiculos extends Controller {
 		render(pessoas);
 	}
 	
+	public static void editar(Long id) {
+		Veiculo v = Veiculo.findById(id);
+		flash.put("v.id", v.id);
+		flash.put("v.placa", v.placa);
+		flash.put("v.ano", v.ano);
+		flash.put("v.modelo", v.modelo);
+		flash.put("v.pessoa.id", v.pessoa.id);
+		form();	
+	}
+	
 	public static void salvar(@Valid Veiculo v) {
 		
 		if (validation.hasErrors()) {
@@ -24,7 +34,7 @@ public class Veiculos extends Controller {
 		}
 		
 		Veiculo vBanco = Veiculo.find("placa = ?1", v.placa).first();
-		if (vBanco != null) {
+		if (vBanco != null && vBanco.id != v.id) {
 			flash.error("Já existe veículo cadastrado com a placa informada");
 			redirecionarErros();
 		}
